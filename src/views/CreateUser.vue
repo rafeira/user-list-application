1dea88dcab2773c96c05f761ab289f0f36c5a66e
rafeira <rafael12wq@gmail.com>
2023-11-19 21:55:51 -0300

    Set users and create user views

---
+++ src/views/CreateUser.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <p>Create user</p>
+  </div>
+</template>
+<style>
+
+</style>
---
+++ src/views/UsersView.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async mounted() {
+    await this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      await axios.get('http://localhost:8080/api/v1/users')
+        .then(response => {
+          this.users = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching users:', error);
+        });
+    },
+
+
+  },
+};
+</script>
+
+<template>
+  <main>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.firstName }} {{ user.lastName }}
+      </li>
+    </ul>
+  </main>
+</template>
