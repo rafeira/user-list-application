abec1f6283ff6d2420523e19ae3ee0684f9645f8
rafeira <rafael12wq@gmail.com>
2023-12-17 10:19:38 -0300

    Set user model

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(firstName = '', lastName = '', id) {
+    this.id = id;
+    this.first_name = firstName;
+    this.last_name = lastName;
+  }
+
+  static fromData(data) {
+    return new User(data.firstName, data.lastName, data.id);
+  }
+
+  fullName() {
+    return `${this.first_name} ${this.last_name}`
+  }
+}
+export default User
