23ac1283126c12b0927573b81ab91ddb28904937
rafeira <rafael12wq@gmail.com>
2023-12-20 20:59:46 -0300

    Add validation to forms

--- src/views/CreateUser.vue
+++ src/views/CreateUser.vue
@@ -1,36 +1,55 @@
 <script>
 import User from '../models/User'
 import UserService from '../services/UserService';
-
+import FormError from '../components/forms/FormError.vue';
 export default {
   data() {
     return {
       user: new User(),
+      errors: {},
       userService: new UserService()
     };
   },
+  components: {
+    FormError
+  },
   methods: {
     async submitForm() {
-      await this.userService.save(this.user)
-      .then((response) => this.$router.push(`${response.data.id}`))
-      .catch((error) => console.error(error));
+      if (this.isFormValid()) {
+        await this.userService.save(this.user)
+          .then((response) => this.$router.push(`${response.data.id}`))
+          .catch((error) => console.error(error));
+      }
+    },
+    isFormValid() {
+      this.errors = {}
+      let isValid = true;
+      if (!this.user.first_name) {
+        this.errors.first_name_presence = "Precisa preencher pelo menos o primeiro nome";
+        isValid = false;
+      }
+      if (this.user.first_name.length < 2) {
+        this.errors.first_name_length = "Primeiro nome precisa ter mais de 1 caractere";
+        isValid = false;
+      }
+      return isValid;
     }
   }
 };
 </script>
 
 <template>
-  <div class="about">
-    <form @submit.prevent="submitForm">
-      <label for="firstName">First Name:</label>
-      <input type="text" id="firstName" v-model="user.first_name">
+  <form @submit.prevent="submitForm">
+    <FormError :error="errors.first_name_presence"/>
+    <FormError :error="errors.first_name_length"/>
+    <label for="firstName">First Name:</label>
+    <input type="text" id="firstName" v-model="user.first_name">
 
-      <label for="lastName">Last Name:</label>
-      <input type="text" id="lastName" v-model="user.last_name">
+    <label for="lastName">Last Name:</label>
+    <input type="text" id="lastName" v-model="user.last_name">
 
-      <button type="submit">Submit</button>
-    </form>
-  </div>
+    <button type="submit">Submit</button>
+  </form>
 </template>
 <style>
 
--- src/views/EditUser.vue
+++ src/views/EditUser.vue
@@ -1,17 +1,23 @@
 <script>
 import User from '../models/User'
 import UserService from '../services/UserService';
+import FormError from '../components/forms/FormError.vue';
 
 export default {
 
   data() {
     return {
       user: new User(),
+      errors: {},
       userService: new UserService()
     };
   },
   async mounted() {
     await this.getUser();
+  },
+
+  components: {
+    FormError
   },
 
   methods: {
@@ -25,9 +31,24 @@ export default {
         });
     },
     async submitForm() {
-      await this.userService.update(this.user)
-      .then((_) => this.$router.push({name: 'show-user', params: {id: this.user.id}}))
-      .catch((error) => console.error(error));
+      if (this.isFormValid()){
+        await this.userService.update(this.user)
+        .then((_) => this.$router.push({name: 'show-user', params: {id: this.user.id}}))
+        .catch((error) => console.error(error));
+      }
+    },
+    isFormValid() {
+      this.errors = {}
+      let isValid = true;
+      if (!this.user.first_name) {
+        this.errors.first_name_presence = "Precisa preencher pelo menos o primeiro nome";
+        isValid = false;
+      }
+      if (this.user.first_name.length < 2) {
+        this.errors.first_name_length = "Primeiro nome precisa ter mais de 1 caractere";
+        isValid = false;
+      }
+      return isValid;
     }
   }
 };
@@ -36,6 +57,8 @@ export default {
 <template>
   <div class="about">
     <form @submit.prevent="submitForm">
+      <FormError :error="errors.first_name_presence"/>
+      <FormError :error="errors.first_name_length"/>
       <label for="firstName">First Name:</label>
       <input type="text" id="firstName" v-model="user.first_name">
 
