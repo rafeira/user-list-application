3abf945bdf7dfb39d296c627121bbe19c108a2f9
rafeira <rafael12wq@gmail.com>
2023-12-20 20:55:22 -0300

    Create form error component

---
+++ src/components/forms/FormError.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    error: String
+  }
+}
+</script>
+
+
+<template>
+  <div v-if="error" class="user-form-error">
+    {{ error }}
+  </div>
+</template>
+
+<style>
+  form .user-form-error{
+    color: red;
+    font-size: 14px;
+  }
+</style>
