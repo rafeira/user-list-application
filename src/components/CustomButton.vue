3e71749edcbd11949cda65eedb019f5155050677
rafeira <rafael12wq@gmail.com>
2023-12-22 20:54:45 -0300

    Create custom button component

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+
+
+<template>
+  <button type="button" class="btn btn-primary" @click="$emit('click')">{{ title }}</button>
+</template>
+
+<style>
+
+</style>
