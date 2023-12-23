b97645320e916e0bd966da12a079a9f2d1bcdf36
rafeira <rafael12wq@gmail.com>
2023-12-22 21:58:28 -0300

    Decorate navbar with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,25 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Users</RouterLink>
-        <RouterLink to="/users/new">Create user</RouterLink>
-      </nav>
-    </div>
+    <nav class="navbar navbar-light navbar-expand-lg border-bottom">
+      <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">Users</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/users/new">Create user</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
-
+  header nav {
+    background-color: rgb(202, 202, 202);
+  }
 </style>
