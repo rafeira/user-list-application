f0ac737ff2551ccfda0e08fa47a4f83039551915
rafeira <rafael12wq@gmail.com>
2023-12-26 20:05:01 -0300

    Change bootstrap importation order

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/js/bootstrap.js';
 const app = createApp(App);
-
 app.use(router);
 
 app.mount("#app");
