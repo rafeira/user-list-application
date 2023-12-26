6534cdc51c233bdfb38dd52025a0d957943e3a53
rafeira <rafael12wq@gmail.com>
2023-12-26 20:04:34 -0300

    Create confirmation modal component

---
+++ src/components/ConfirmationModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="modal fade" :id="modalId" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="confirmationModalLabel">{{ title }}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          {{ contentMessage }}
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Não</button>
+          <button type="button" @click.prevent="this.onConfirmButtonPressed()" :class="`btn btn-primary ${confirmationButtonColor}`" data-bs-dismiss="modal">{{ confirmationButtonTitle }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: "Confirm?"
+      },
+      contentMessage: {
+        type: String,
+        default: "Are you sure do you wanna proceed?"
+      },
+      modalId: String,
+      confirmationButtonTitle: {
+        type: String,
+        default: "Sim"
+      },
+      confirmationButtonColor: String
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      onConfirmButtonPressed() {
+        this.$emit('confirm-action');
+      }
+    }
+  }
+</script>
