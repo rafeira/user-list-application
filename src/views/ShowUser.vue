1f27d4e5ae8edf8c1dd4e7e1fd42c54114845bf2
rafeira <rafael12wq@gmail.com>
2023-12-26 22:51:43 -0300

    Style show user route

--- src/views/ShowUser.vue
+++ src/views/ShowUser.vue
@@ -1,4 +1,6 @@
 <script>
+import ConfirmationModal from '../components/ConfirmationModal.vue';
+import CustomButton from '../components/CustomButton.vue';
 import User from '../models/User';
 import UserService from '../services/UserService';
 
@@ -16,34 +18,43 @@ export default {
     async getUser() {
       await this.userService.find(this.$route.params.id)
         .then(user => {
-          this.user = user;
-        })
+        this.user = user;
+      })
         .catch(error => {
-          console.error('Error fetching user:', error);
-        });
+        console.error('Error fetching user:', error);
+      });
     },
     async removeUser() {
       await this.userService.remove(this.user.id)
-        .then((_) => this.$router.push({name: "users"}))
+        .then((_) => this.$router.push({ name: "users" }))
         .catch((error) => {
-          console.error(error)
-        });
+        console.error(error);
+      });
     },
-    async onEditUserPressed(userId) {
-      this.$router.push({path: `users/${this.user.id}/edit`, name: 'edit-user'})
+    async onEditUserPressed() {
+      this.$router.push({ path: `users/${this.user.id}/edit`, name: 'edit-user' });
     },
-
   },
+  components: { CustomButton, ConfirmationModal }
 };
 </script>
 
 <template>
   <main>
-    <p>
-      {{ user.fullName() }}
-    </p>
-    <button @click="this.removeUser()">Remove</button>
-    <button @click="this.onEditUserPressed()">Edit</button>
-
+    <div id="user-container">
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <p>
+          {{ user.fullName() }}
+        </p>
+        <CustomButton @click="this.onEditUserPressed()" title="Edit"/>
+        <CustomButton title="Remove" class="btn-danger" data-bs-toggle="modal" data-bs-target="#removeConfirmationModal"/>
+        <ConfirmationModal @confirm-action="this.removeUser()" modal-id="removeConfirmationModal" confirmation-button-color="btn-danger"/>
+      </div>
+    </div>
   </main>
 </template>
+<style>
+  #user-container {
+    margin: 100px 50px;
+  }
+</style>
